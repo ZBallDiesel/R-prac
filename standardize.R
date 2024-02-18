3b013c2896db8e6d0bc32147249a5083c8e99b16
Zach Turner <zachturner@berkeley.edu>
2024-02-17 22:31:25 -0800

    test

---
+++ standardize.R
@@ -0,0 +1,16 @@
+#' Title
+#'
+#' @param data A numerical vector to be standardized.
+#'
+#' @return A vector that is the standardized version of data.
+#' @export
+#'
+#' @examples
+#' x <- c(1,0,1,3,3)
+#' standardize(x)
+#'
+standardize <- function(data){
+  mu = mean(data)
+  sigma = sd(data)
+  return((data - mu)/sigma)
+}
